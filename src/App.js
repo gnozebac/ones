5e5332073244356bc6fbae5b800da5999d235bac
gnozebac <magbmail@gmail.com>
2021-10-13 10:29:53 -0500

    deploy 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Splash from './pages/Splash'
 import Search from './pages/Search'
 import Weight from './pages/Weight'
@@ -18,7 +18,7 @@ function App() {
 
     <BrowserRouter>
       <Switch>
-      <Route exact path="/" component={Splash} />   
+      <Route exact path="/" > <Redirect to="/splash" />  </Route>
         <Route exact path="/splash" component={Splash} />        
         <Route exact path="/search" component={Search} />        
         <Route exact path="/weight/:current" component={Weight} />        
